73d3c878386b03f9a2e56630bd4f97fb486efb25
subhasishnath45 <subhasishnath45@gmail.com>
2021-01-25 18:20:20 +0530

    Committed on 25th Jan 2021

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// function get_user(){
+//     window.setTimeout(function(){
+//         var u = {
+//             'name':'CAD Centre'
+//         };
+//         return u;
+//         // callback(u);
+//     }, 5000);
+// }
+
+function greet_user(user){
+    console.log('Hi ' + user.name + ', how are you?');
+}
+
+User = {
+    'name':''
+}
+
+try{
+    if(!User.name){
+        throw 'Name is empty';
+    }
+    greet_user(User);
+}catch(error){
+    console.log(error);
+}finally{
+    console.log("A new line of code");
+}
+
+// get_user(greet_user);
+
+// get_user(function (user){
+//     console.log('Hi ' + user.name + ', how are you?');
+// })
+
+// greet_user(); = I'm calling greet_user function.
+// greet_user; = function (user){
+//     console.log('Hi ' + user.name + ', how are you?');
+// }
+
+// Error Handling.
+
+
+
+// try{
+//     // try some code
+// }catch(error){
+//     console.log(error);
+// }finally{
+
+// }
+
+
+
+
